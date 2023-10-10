cc5539928df10512a02fb87c576f0433e91b212a
Uzair <109786747+uzairansari0@users.noreply.github.com>
2023-10-10 15:37:54 +0530

    Update 2_📕_page.py

--- pages/2_📕_page.py
+++ pages/2_📕_page.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import plotly.graph_objects as go
 
 st.set_page_config(page_title="Super Store Data ",
                    page_icon=":bar_chart:",
