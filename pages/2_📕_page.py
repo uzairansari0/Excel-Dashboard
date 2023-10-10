2a16d0a01e634109fd6225ae249d30c3857205c4
Uzair <109786747+uzairansari0@users.noreply.github.com>
2023-10-10 15:37:14 +0530

    Update 2_📕_page.py

--- pages/2_📕_page.py
+++ pages/2_📕_page.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.set_page_config(page_title="Sales Dashboard Page 1",
+st.set_page_config(page_title="Super Store Data ",
                    page_icon=":bar_chart:",
                    layout="wide")
 
